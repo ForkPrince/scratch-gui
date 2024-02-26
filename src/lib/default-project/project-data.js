2c69c34d55f12b9884c6e17e5637b8a2791fcebb
Prince527 <Prince527gaming@gmail.com>
2024-02-26 18:05:48 -0400

    Changed the default project

--- src/lib/default-project/project-data.js
+++ src/lib/default-project/project-data.js
@@ -11,6 +11,11 @@ let messages = defineMessages({
         defaultMessage: 'my variable',
         description: 'Name for the default variable',
         id: 'gui.defaultProject.variable'
+    },
+    comment_1: {
+        defaultMessage: 'Thanks for using ServerSMP!\n- Load from localhost:3000\n- Hidden blocks\n- New extensions',
+        description: 'ServerSMP comment',
+        id: 'gui.defaultProject.comment_1'
     }
 });
 
@@ -27,96 +32,137 @@ const defaultTranslator = msgObj => msgObj.defaultMessage;
 const projectData = translateFunction => {
     const translator = translateFunction || defaultTranslator;
     return ({
-        targets: [
-            {
-                isStage: true,
-                name: 'Stage',
-                variables: {
-                    '`jEk@4|i[#Fk?(8x)AV.-my variable': [
-                        translator(messages.variable),
-                        0
-                    ]
-                },
-                lists: {},
-                broadcasts: {},
-                blocks: {},
-                currentCostume: 0,
-                costumes: [
-                    {
-                        assetId: 'cd21514d0531fdffb22204e0ec5ed84a',
-                        name: translator(messages.backdrop, {index: 1}),
-                        md5ext: 'cd21514d0531fdffb22204e0ec5ed84a.svg',
-                        dataFormat: 'svg',
-                        rotationCenterX: 240,
-                        rotationCenterY: 180
-                    }
-                ],
-                sounds: [
-                    {
-                        assetId: '83a9787d4cb6f3b7632b4ddfebf74367',
-                        name: translator(messages.pop),
-                        dataFormat: 'wav',
-                        format: '',
-                        rate: 11025,
-                        sampleCount: 258,
-                        md5ext: '83a9787d4cb6f3b7632b4ddfebf74367.wav'
-                    }
-                ],
-                volume: 100
+        targets: [{
+            isStage: true,
+            name: 'Stage',
+            variables: {
+                '`jEk@4|i[#Fk?(8x)AV.-my variable': [
+                    translator(messages.variable),
+                    0
+                ]
             },
-            {
-                isStage: false,
-                name: translator(messages.sprite, {index: 1}),
-                variables: {},
-                lists: {},
-                broadcasts: {},
-                blocks: {},
-                currentCostume: 0,
-                costumes: [
-                    {
-                        assetId: 'bcf454acf82e4504149f7ffe07081dbc',
-                        name: translator(messages.costume, {index: 1}),
-                        bitmapResolution: 1,
-                        md5ext: 'bcf454acf82e4504149f7ffe07081dbc.svg',
-                        dataFormat: 'svg',
-                        rotationCenterX: 48,
-                        rotationCenterY: 50
+            lists: {},
+            broadcasts: {},
+            blocks: {},
+            comments: {},
+            currentCostume: 0,
+            costumes: [{
+                name: translator(messages.backdrop, {index: 1}),
+                bitmapResolution: 1,
+                dataFormat: 'svg',
+                assetId: 'cd21514d0531fdffb22204e0ec5ed84a',
+                md5ext: 'cd21514d0531fdffb22204e0ec5ed84a.svg',
+                rotationCenterX: 0,
+                rotationCenterY: 0
+            }],
+            sounds: [{
+                name: translator(messages.pop),
+                assetId: '83a9787d4cb6f3b7632b4ddfebf74367',
+                dataFormat: 'wav',
+                format: '',
+                rate: 48000,
+                sampleCount: 1124,
+                md5ext: '83a9787d4cb6f3b7632b4ddfebf74367.wav'
+            }],
+            volume: 100,
+            layerOrder: 0,
+            tempo: 60,
+            videoTransparency: 50,
+            videoState: 'on',
+            textToSpeechLanguage: null
+        }, {
+            isStage: false,
+            name: translator(messages.sprite, {index: 1}),
+            variables: {},
+            lists: {},
+            broadcasts: {},
+            blocks: {
+                a: {
+                    opcode: 'argument_reporter_boolean',
+                    next: null,
+                    parent: null,
+                    inputs: {},
+                    fields: {
+                        VALUE: ['is forkphorus?', null]
                     },
-                    {
-                        assetId: '0fb9be3e8397c983338cb71dc84d0b25',
-                        name: translator(messages.costume, {index: 2}),
-                        bitmapResolution: 1,
-                        md5ext: '0fb9be3e8397c983338cb71dc84d0b25.svg',
-                        dataFormat: 'svg',
-                        rotationCenterX: 46,
-                        rotationCenterY: 53
-                    }
-                ],
-                sounds: [
-                    {
-                        assetId: '83c36d806dc92327b9e7049a565c6bff',
-                        name: translator(messages.meow),
-                        dataFormat: 'wav',
-                        format: '',
-                        rate: 22050,
-                        sampleCount: 18688,
-                        md5ext: '83c36d806dc92327b9e7049a565c6bff.wav'
-                    }
-                ],
-                volume: 100,
-                visible: true,
-                x: 0,
-                y: 0,
-                size: 100,
-                direction: 90,
-                draggable: false,
-                rotationStyle: 'all around'
-            }
-        ],
+                    shadow: false,
+                    topLevel: true,
+                    x: 43,
+                    y: 616
+                },
+                b: {
+                    opcode: 'argument_reporter_boolean',
+                    next: null,
+                    parent: null,
+                    inputs: {},
+                    fields: {
+                        VALUE: ['is TurboWarp?', null]
+                    },
+                    shadow: false,
+                    topLevel: true,
+                    x: 226,
+                    y: 616
+                },
+                c: {
+                    opcode: 'argument_reporter_boolean',
+                    next: null,
+                    parent: null,
+                    inputs: {},
+                    fields: {
+                        VALUE: ['is compiled?', null]
+                    },
+                    shadow: false,
+                    topLevel: true,
+                    x: -133,
+                    y: 613
+                }
+            },
+            comments: {
+                f: {
+                    blockId: null,
+                    x: -69,
+                    y: 305,
+                    width: 377,
+                    height: 244,
+                    minimized: false,
+                    text: translator(messages.comment_1)
+                }
+            },
+            currentCostume: 0,
+            costumes: [{
+                name: 'welcome',
+                bitmapResolution: 1,
+                dataFormat: 'svg',
+                assetId: 'f083ba67427a2da8d308a8a2e6ac7626',
+                md5ext: 'f083ba67427a2da8d308a8a2e6ac7626.svg',
+                rotationCenterX: 191.69610845721985,
+                rotationCenterY: 143.57681313291286
+            }],
+            sounds: [{
+                name: translator(messages.pop),
+                assetId: '83a9787d4cb6f3b7632b4ddfebf74367',
+                dataFormat: 'wav',
+                format: '',
+                rate: 48000,
+                sampleCount: 1124,
+                md5ext: '83a9787d4cb6f3b7632b4ddfebf74367.wav'
+            }],
+            volume: 100,
+            layerOrder: 1,
+            visible: true,
+            x: 0,
+            y: 0,
+            size: 100,
+            direction: 90,
+            draggable: false,
+            rotationStyle: 'all around'
+        }],
+        monitors: [],
+        extensions: [],
         meta: {
             semver: '3.0.0',
-            vm: '0.1.0',
-            agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36' // eslint-disable-line max-len
+            vm: '0.2.0',
+            agent: ''
         }
     });
 };
